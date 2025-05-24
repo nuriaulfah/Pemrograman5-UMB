7545912f2de432e592249c8ff32455e5c8b6f13b
nuriaulfah <nuriaulfah300104@gmail.com>
2025-05-24 15:20:59 +0700

    add file project

---
+++ Assets/GerakanPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerakanPlayer : MonoBehaviour
+{
+	public float kecepatan;
+	Rigidbody rb;
+	Animator anim;
+	
+	//sess 3
+	public Transform playerPutaran;
+	
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Berjalan();
+    }
+	
+	void Berjalan()
+	{
+		float jalan = Input.GetAxis("Horizontal");
+		rb.linearVelocity = Vector3.right * jalan * kecepatan;
+		//session 2
+		anim.SetFloat("Kecepatan", Mathf.Abs(jalan), 0.1f, Time.deltaTime);
+		//sess 3
+		playerPutaran.localEulerAngles = new Vector3(0, jalan * 90, 0);
+	}
+	
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.collider.CompareTag("virus"))
+		{
+			Time.timeScale = 0;
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/MunculinVirus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunculinVirus : MonoBehaviour
+{
+	public GameObject virus;
+	public float waktuMinimal, waktuMaksimal;
+	public float posisiMinimal, posisiMaksimal; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MunculVirus());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	IEnumerator MunculVirus()
+	{
+		Instantiate(virus, transform.position + Vector3.right * Random.Range(posisiMinimal, posisiMaksimal), Quaternion.identity);
+		yield return new WaitForSeconds(Random.Range(waktuMinimal, waktuMaksimal));
+		StartCoroutine(MunculVirus());
+	}
+}
